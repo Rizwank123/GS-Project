4bc323cd05cfc6488f83739be210747176b021f2
Shashikant Bharti <surya.indian321@gmail.com>
2022-02-19 13:19:51 +0530

    Owl Carousel Added

---
+++ index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GS Service Center</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+        integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- == Navbar == -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container-fluid">
+            <a class="navbar-brand fs-4" style="color:red" href="#">GS Service Center</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#"> <i class="bi bi-briefcase"></i> Become a
+                            Partner</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#"> <i class="bi bi-bag"></i> Become a
+                            Customer</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#"> <i class="bi bi-cart2"></i> My Bookings </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#"> <i class="bi bi-person-circle"></i> SignIn /
+                            SignUp</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- == //Navbar == -->
+    <!-- Search Bar -->
+    <div class="container-fluid bg-light">
+        <form action="#">
+            <div class="container py-4">
+                <div class="row">
+                    <div class="col-md-10 my-1">
+                        <input type="search" name="search" id="search" class="form-control"
+                            placeholder="Search Here...">
+                    </div>
+                    <div class="col-md-2 my-1">
+                        <button type="submit" name="search-btm" class="btn btn-danger w-100">Search</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+    <!-- //Search Bar -->
+
+    <!-- Banner -->
+    <div class="container my-3">
+        <div class="owl-carousel owl-theme owl-loaded">
+            <div class="owl-stage-outer">
+                <div class="owl-stage">
+                    <div class="owl-item">
+                        <a href="#">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="https://i.pinimg.com/originals/55/4c/c8/554cc8e970af8e6f6073b9a36efe0ca1.jpg"
+                                        alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="owl-item">
+                        <a href="#">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf9WxZoJrJQzMVOdGJF66SJYBvcVFYk0c2nikDRnloQ3Qf_yg02ZqstAGjg7n-AjTPoeA&usqp=CAU"
+                                        alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="owl-item">
+                        <a href="#">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThvm00_MgljqdfzcIRWuLOR6Lg0jA47GxOIy6sXCzobOCAUv8Xn_hTOtzDNl0sa0q8yC4&usqp=CAU"
+                                        alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="owl-item">
+                        <a href="#">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd0l64cLA-skWXRgUp-hFrpflfWEiiXyck75q1a8OUwVTd4VBVhI0MrV2tqSTTRIg63PI&usqp=CAU"
+                                        alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="owl-item">
+                        <a href="#">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3zugSlYtq2sHKza4U_VbKFob9zRXY69Ffu420VdhSEhNX9mX1FQB3EEdOt0ye0rR7epE&usqp=CAU"
+                                        alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="owl-item">
+                        <a href="#">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvzsEqKUApeaq_IwfofiTypOVIri-7m4eRXy5rDRp3lno65lhVHqRGhwQd9fNikjQSn3k&usqp=CAU"
+                                        alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+           
+        </div>
+    </div>
+
+    <!-- //Banner -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+        integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+    $(window).on('load', function() {
+        $('#preloader').delay(2000).fadeOut('slow');
+        $('body').delay(1000).css({
+            'overflow': 'visible'
+        });
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 4,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+    </script>
+</body>
+
+</html>
